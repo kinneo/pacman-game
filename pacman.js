c39ecee54afe9895704ae966515ecab29c78e036
kinneo <kinneo2002@gmail.com>
2025-07-03 15:30:56 +0800

    wraparndhotfixxxxxxxxxxxxxxx

--- pacman.js
+++ pacman.js
@@ -682,12 +682,12 @@ function move(){
     const halfwayOutLeft = pacman.x < 0 && pacman.x + pacman.width > 0;
     const halfwayOutRight = pacman.x < boardWidth && pacman.x + pacman.width > boardWidth;
 
-    if ((halfwayOutLeft || halfwayOutRight)) {
+    if ((!halfwayOutLeft && !halfwayOutRight)) {
         applyWrapAround(pacman);
         //return; // Skip applyWrapAround if halfway through tunnel
     }
 
-    applyWrapAround(pacman); // could it be how i call this function? isit being continuosly called? shld i add a condtion b4 calling this fn instead?
+    //applyWrapAround(pacman); // could it be how i call this function? isit being continuosly called? shld i add a condtion b4 calling this fn instead?
 }
 
 function movePacman(e){
